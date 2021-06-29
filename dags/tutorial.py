b95fbb18b5aaf65f6a962e90bb2a38f0ac3b11d7
anuj jain <anuj.j@razorpay.com>
2021-06-29 12:54:22 +0530

    fix

--- dags/tutorial.py
+++ dags/tutorial.py
@@ -32,7 +32,7 @@ default_args = {
     # 'trigger_rule': 'all_success'
 }
 with DAG(
-    'tutorial',
+    'test',
     default_args=default_args,
     description='A simple tutorial DAG',
     schedule_interval=timedelta(days=1),

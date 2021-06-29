a63445fc301b854017287581815ea04ce719e668
anuj jain <anuj.j@razorpay.com>
2021-06-29 12:36:38 +0530

    fix

--- dags/test.py
+++ dags/test.py
@@ -6,14 +6,14 @@ from airflow.operators.python import PythonOperator
 
 from lib import email, presto
 
-DAG_OWNER = 'sumanth.psv@razorpay.com'
+DAG_OWNER = 'test123'
 
 default_args = {
     'owner': DAG_OWNER,
     'start_date': datetime(2021, 6, 2, 9, 30, 0),
     'provide_context': True,
     'on_failure_callback': partial(email.send_mail_slack,
-                                   ['shiva.vippa@razorpay.com', 'product.analytics@razorpay.com'], ["datahub"]),
+                                   ['test@gmail.com']),
     'retries': 2,
     'retry_delay': timedelta(seconds=300)
 }
@@ -30,11 +30,3 @@ sql1 = """
 drop table if exists hive.test.test123;
 """
 
-presto_task1 = PythonOperator(
-    task_id='ExecutePrestoQuery1',
-    provide_context=True,
-    python_callable=presto.execute_presto_query_cli,
-    op_kwargs={'query': sql1, 'user': DAG_OWNER},
-    dag=dag)
-
-presto_task1
